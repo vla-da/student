e1413724e4e86151be0f72479150cffa5724c6e4
vla-da <64595423+vla-da@users.noreply.github.com>
2020-12-13 16:36:07 +0500

    Update delete.php

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?require_once 'engine/page/title.php';?>
+<?require_once 'engine/connection/connectionStart.php';?>
+<html>
+    <body>
+		<?
+			if(isset($_GET['id'])&&isset($_GET['query'])){
+                $id = htmlentities(mysqli_real_escape_string($link, $_GET['id']));
+                $index = htmlentities(mysqli_real_escape_string($link, $_GET['query']));
+                if($index=="vedm_info"){
+                    $index="vedm";
+                }
+                $query = "DELETE FROM $database.$index WHERE id='$id'";
+                $result = mysqli_query($link, $query) or die ("Ошибка в запросе");
+                header("Location: index.php");
+			}
+		?>
+	</body>
+</html>
+<?require_once 'engine/connection/connectionEnd.php';?>
